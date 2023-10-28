bd142c121d645c14ac52f805b0d242f1a4f3aa9a
Sneha Sasmal <snehasasmal2003@gmail.com>
2023-10-28 19:27:44 +0530

    added food delivery web code

---
+++ firstproj.js
@@ -0,0 +1,9 @@
+function onloadpage() {
+document.getElementById('coupon').style.visibility = 'visible';
+document.getElementByClassName('mainbaradandoffers').style.opacity = '0.1'
+}
+function  closecoupon() {
+    document.getElementById('coupon').style.visibility = 'hidden';
+    document.getElementByClassName('mainbaradandoffers').style.opacity = '1'
+    }
+   
